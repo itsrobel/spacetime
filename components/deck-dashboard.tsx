3eabfa67a41f5f2431dd91186f0f6e4847a59d54
Robel Schwarz <itsrobel.schwarz@gmail.com>
2025-04-06 03:28:31 -0400

    broken trpc impl

--- components/deck-dashboard.tsx
+++ components/deck-dashboard.tsx
@@ -1,11 +1,12 @@
 "use client";
+
 import { Deck, Flash } from "@/prisma/client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Dialog,
   DialogContent,
@@ -13,194 +14,371 @@ import {
   DialogFooter,
   DialogTitle,
 } from "@/components/ui/dialog";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { Input } from "@/components/ui/input";
 
 import DeckCard from "@/components/deck-card";
-import FlashcardView from "@/components/flashcard-view";
 import { useIsMobile } from "@/hooks/use-mobile";
 
-export default function DeckDashboard() {
+const fetchData = async (url: string) => {
+  const res = await fetch(url);
+  const json = await res.json();
+  return json.data;
+};
+
+const DeckDialog = ({
+  isOpen,
+  onClose,
+  selectedDeck,
+}: {
+  isOpen: boolean;
+  onClose: (isOpen: boolean) => void;
+  selectedDeck: Deck | null;
+}) => (
+  <Dialog open={isOpen} onOpenChange={onClose}>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>{selectedDeck?.name}</DialogTitle>
+      </DialogHeader>
+      {selectedDeck && (
+        <div className="flex flex-col items-center">
+          {/* Add FlashcardView or relevant content here */}
+        </div>
+      )}
+      <DialogFooter></DialogFooter>
+    </DialogContent>
+  </Dialog>
+);
+
+const FlashDialog = ({
+  isOpen,
+  onClose,
+  selectedFlash,
+}: {
+  isOpen: boolean;
+  onClose: (isOpen: boolean) => void;
+  selectedFlash: Flash | null;
+}) => (
+  <Dialog open={isOpen} onOpenChange={onClose}>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>{selectedFlash?.title}</DialogTitle>
+      </DialogHeader>
+      {selectedFlash && (
+        <div className="flex flex-col items-center">
+          {selectedFlash?.id}
+          {/* Add FlashcardView or relevant content here */}
+        </div>
+      )}
+      <DialogFooter></DialogFooter>
+    </DialogContent>
+  </Dialog>
+);
+
+const CreateFlashDialog = ({
+  isOpen,
+  onClose,
+  onSave,
+}: {
+  isOpen: boolean;
+  onClose: (isOpen: boolean) => void;
+  onSave: (title: string, content: string) => void;
+}) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleSave = () => {
+    onSave(title, content);
+    setTitle("");
+    setContent("");
+    onClose(false);
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create New Flash</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="deck-name" className="text-right">
+              title
+            </Label>
+            <Input
+              id="deck-name"
+              value={title}
+              className="col-span-3"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="deck-name" className="text-right">
+              content
+            </Label>
+            <Input
+              id="deck-name"
+              value={content}
+              className="col-span-3"
+              onChange={(e) => setContent(e.target.value)}
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit" onClick={handleSave}>
+            Save changes
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const CreateDeckDialog = ({
+  isOpen,
+  onClose,
+  onSave,
+}: {
+  isOpen: boolean;
+  onClose: (isOpen: boolean) => void;
+  onSave: (deckName: string) => void;
+}) => {
+  const [deckName, setDeckName] = useState("");
+
+  const handleSave = () => {
+    onSave(deckName);
+    setDeckName("");
+    onClose(false);
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create New Deck</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="deck-name" className="text-right">
+              Name
+            </Label>
+            <Input
+              id="deck-name"
+              value={deckName}
+              className="col-span-3"
+              onChange={(e) => setDeckName(e.target.value)}
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit" onClick={handleSave}>
+            Save changes
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const DeckGrid = ({ decks }: { decks: Deck[] | undefined }) => {
   const router = useRouter();
   const isMobile = useIsMobile();
-  const [decks, setDecks] = useState<Deck[]>();
-  const [flashcards, setFlashCards] = useState<Flash[]>();
 
   const [selectedDeck, setSelectedDeck] = useState<Deck | null>(null);
-  const [currentCardIndex, setCurrentCardIndex] = useState(0);
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [newDeckDialogOpen, setNewDeckDialogOpen] = useState(false);
-
-  const [newDeckName, setNewDeckName] = useState("");
-
-  const getDeck = async () => {
-    const res = await fetch("/api/deck");
-    const json = await res.json();
-    console.log(json);
-    setDecks(json.data);
-  };
+  const [isDeckDialogOpen, setIsDeckDialogOpen] = useState(false);
 
   const handleDeckClick = (deck: Deck) => {
     if (isMobile) {
       router.push(`/deck/${deck.id}`);
     } else {
       setSelectedDeck(deck);
-      setCurrentCardIndex(0);
-      setIsDialogOpen(true);
+      setIsDeckDialogOpen(true);
     }
   };
-  const handleNewDeck = async () => {
-    // console.log(newDeckName);
-    const res = await fetch("/api/deck", {
-      method: "POST",
-      body: JSON.stringify({ name: newDeckName }),
-    });
-    const json = await res.json();
-    console.log(json);
-
-    setNewDeckDialogOpen(false);
-  };
-
-  // const handleNextCard = () => {
-  //   if (selectedDeck && currentCardIndex < selectedDeck.cards.length - 1) {
-  //     setCurrentCardIndex(currentCardIndex + 1);
-  //   }
-  // };
-
-  const handlePrevCard = () => {
-    if (currentCardIndex > 0) {
-      setCurrentCardIndex(currentCardIndex - 1);
-    }
-  };
-  useEffect(() => {
-    if (!decks) {
-      getDeck();
-    }
-  });
 
   return (
-    <div className="w-full max-w-6xl">
-      <div className="mb-6 flex justify-between">
-        <h2 className="text-xl font-semibold">Your Decks</h2>
-        <Button
-          size="sm"
-          onClick={() => {
-            setNewDeckDialogOpen(true);
-          }}
-        >
-          <Plus className="mr-2 h-4 w-4" />
-          New Deck
-        </Button>
-
-        <Button
-          size="sm"
-          onClick={async () => {
-            const cards = await fetch("/api/flash", {
-              method: "POST",
-              body: JSON.stringify({
-                deck: selectedDeck?.id,
-                title: "test flash",
-                content: "test flash content",
-              }),
-            });
-          }}
-        >
-          <Plus className="mr-2 h-4 w-4" />
-          FlashCard
-        </Button>
-      </div>
+    <div>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {decks &&
-          decks.map((deck) => (
-            <DeckCard
-              key={deck.id}
-              deck={deck}
-              onClick={() => handleDeckClick(deck)}
-            />
-          ))}
+        {decks?.map((deck) => (
+          <DeckCard
+            key={deck.id}
+            deck={deck}
+            onClick={() => handleDeckClick(deck)}
+          />
+        ))}
       </div>
-      {/* Desktop Modal View */}
-      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>{selectedDeck?.name}</DialogTitle>
-          </DialogHeader>
-          {selectedDeck && (
-            <div className="flex flex-col items-center">
-              {!flashcards && <div>create flash</div>}
 
-              {/* <FlashcardView */}
-              {/*   card={selectedDeck.cards[currentCardIndex]} */}
-              {/*   index={currentCardIndex} */}
-              {/*   total={selectedDeck.cards.length} */}
-              {/*   onNext={handleNextCard} */}
-              {/*   onPrev={handlePrevCard} */}
-
-              {/* /> */}
-            </div>
-          )}
-          <DialogFooter></DialogFooter>
-        </DialogContent>
-      </Dialog>
-
-      <Dialog open={newDeckDialogOpen} onOpenChange={setNewDeckDialogOpen}>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Create New Deck</DialogTitle>
-            {/* <DialogDescription> */}
-            {/*   Create a new changes to your profile here. Click save when you're done. */}
-            {/* </DialogDescription> */}
-          </DialogHeader>
-          {/* maybe add like deck tags or something latter based of subjects */}
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="text-right">
-                Name
-              </Label>
-              <Input
-                id="name"
-                value={newDeckName}
-                className="col-span-3"
-                onChange={(e) => setNewDeckName(e.target.value)}
-              />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit" onClick={handleNewDeck}>
-              Save changes
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-
-      <Dialog open={newDeckDialogOpen} onOpenChange={setNewDeckDialogOpen}>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Create New Deck</DialogTitle>
-            {/* <DialogDescription> */}
-            {/*   Create a new changes to your profile here. Click save when you're done. */}
-            {/* </DialogDescription> */}
-          </DialogHeader>
-          {/* maybe add like deck tags or something latter based of subjects */}
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="text-right">
-                Name
-              </Label>
-              <Input
-                id="name"
-                value={newDeckName}
-                className="col-span-3"
-                onChange={(e) => setNewDeckName(e.target.value)}
-              />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit" onClick={handleNewDeck}>
-              Save changes
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+      <DeckDialog
+        isOpen={isDeckDialogOpen}
+        onClose={setIsDeckDialogOpen}
+        selectedDeck={selectedDeck}
+      />
     </div>
   );
+};
+const FlashcardTable = ({
+  flashcards,
+}: {
+  flashcards: Flash[] | undefined;
+}) => {
+  // const router = useRouter();
+  const isMobile = useIsMobile();
+
+  const [selectedFlash, setSelectedFlash] = useState<Flash | null>(null);
+
+  const [isFlashDialogOpen, setIsFlashDialogOpen] = useState(false);
+
+  const handleFlashClick = (flash: Flash) => {
+    if (isMobile) {
+      console.log("create mobile page");
+      // router.push(`/deck/${deck.id}`);
+    } else {
+      setSelectedFlash(flash);
+      setIsFlashDialogOpen(true);
+    }
+  };
+  // const addToDeck = () = {
+  //
+  // }
+
+  return (
+    <div>
+      <Table>
+        <TableCaption>A list of your flashcards.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableCell>Title</TableCell>
+            <TableCell className="text-right">Content</TableCell>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {flashcards?.map((flash) => (
+            <TableRow key={flash.id} onClick={() => handleFlashClick(flash)}>
+              <TableCell>{flash.title}</TableCell>
+              <TableCell className="text-right">{flash.content}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <FlashDialog
+        isOpen={isFlashDialogOpen}
+        onClose={setIsFlashDialogOpen}
+        selectedFlash={selectedFlash}
+      />
+    </div>
+  );
+};
+
+export default function DeckDash() {
+  const [decks, setDecks] = useState<Deck[]>();
+  const [flashcards, setFlashcards] = useState<Flash[]>();
+  const [isNewDeckDialogOpen, setIsNewDeckDialogOpen] = useState(false);
+  const [isNewFlashDialogOpen, setIsNewFlashDialogOpen] = useState(false);
+
+  const loadData = async () => {
+    if (!decks || !flashcards) {
+      setDecks(await fetchData("/api/deck"));
+      setFlashcards(await fetchData("/api/flash"));
+    }
+  };
+
+  const handleCreateNewDeck = async (name: string) => {
+    await fetch("/api/deck", {
+      method: "POST",
+      body: JSON.stringify({ name }),
+    });
+    setIsNewDeckDialogOpen(false);
+    loadData();
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-4 md:p-8">
+      <div className="w-full">
+        {/* Action Buttons */}
+        <div className="mb-6 flex items-center gap-4">
+          <Button onClick={() => setIsNewDeckDialogOpen(true)}>
+            <Plus className="h-4 w-4" />
+            Create Deck
+          </Button>
+
+          <Button onClick={() => setIsNewFlashDialogOpen(true)}>
+            <Plus className="h-4 w-4" />
+            Create FlashCard
+          </Button>
+        </div>
+
+        {/* Tabs */}
+        <Tabs defaultValue="decks" className="w-full">
+          <TabsList>
+            <TabsTrigger value="decks">Decks</TabsTrigger>
+            <TabsTrigger value="flash">FlashCards</TabsTrigger>
+          </TabsList>
+
+          {/* Decks Tab */}
+          <TabsContent value="decks">
+            <DeckGrid decks={decks} />
+          </TabsContent>
+
+          {/* Flashcards Tab */}
+          <TabsContent value="flash">
+            <FlashcardTable flashcards={flashcards} />
+          </TabsContent>
+        </Tabs>
+
+        {/* Modals */}
+        <CreateDeckDialog
+          isOpen={isNewDeckDialogOpen}
+          onClose={setIsNewDeckDialogOpen}
+          onSave={handleCreateNewDeck}
+        />
+
+        <CreateFlashDialog
+          isOpen={isNewFlashDialogOpen}
+          onClose={setIsNewFlashDialogOpen}
+          onSave={() => {}}
+        />
+      </div>
+    </main>
+  );
 }
+// {selectedDeck && (
+//   <div className="flex flex-col items-center">
+//     {!flashcards && <div>create flash</div>}
+//
+{
+  /* <FlashcardView */
+}
+{
+  /*   card={selectedDeck.cards[currentCardIndex]} */
+}
+{
+  /*   index={currentCardIndex} */
+}
+{
+  /*   total={selectedDeck.cards.length} */
+}
+{
+  /*   onNext={handleNextCard} */
+}
+{
+  /*   onPrev={handlePrevCard} */
+}
+
+{
+  /* /> */
+}
+//   </div>
+// )}
